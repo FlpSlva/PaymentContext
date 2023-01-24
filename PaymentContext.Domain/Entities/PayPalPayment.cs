6d1c2ca09467c8fead6215a58725e2abfef5a58f
FlpSlva <flpslvadev@gmail.com>
2023-01-24 09:08:00 -0300

    correcao na entidade

--- PaymentContext.Domain/Entities/PayPalPayment.cs
+++ PaymentContext.Domain/Entities/PayPalPayment.cs
@@ -10,13 +10,13 @@ namespace PaymentContext.Domain.Entities
     public class PaypalPayment : Payment
     {
         public PaypalPayment(
-            string email,
+            Email email,
             string transactionCode,
             DateTime paidDate,
             DateTime expireDate,
             decimal total,
             decimal totalPaid,
-            string address,
+            Address address,
             Document document,
             string payer) : base(paidDate,
                 expireDate,
