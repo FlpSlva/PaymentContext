0eddd12c47c5c5a82eee7f2db09f12392ad487aa
FlpSlva <flpslvadev@gmail.com>
2023-01-26 09:40:19 -0300

    criando testes de document

---
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,44 @@
+﻿using PaymentContext.Domain.ValuesObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Tests.ValueObjects
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        // Red, Green, Refactor
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenTheCNPJIsInvalid()
+        {
+            var doc = new Document("123", Domain.Enums.EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnSuccessWhenTheCNPJIsValid()
+        {
+            var doc = new Document("27782554000144", Domain.Enums.EDocumentType.CNPJ);
+            Assert.IsTrue(doc.Valid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnErrorWhenTheCPFIsInvalid()
+        {
+            var doc = new Document("123", Domain.Enums.EDocumentType.CPF);
+            Assert.IsTrue(doc.Invalid);
+        }
+
+        [TestMethod]
+        public void ShouldReturnSuccessWhenTheCPFIsValid()
+        {
+            var doc = new Document("95617072412", Domain.Enums.EDocumentType.CPF);
+            Assert.IsTrue(doc.Valid);
+        }
+
+    }
+}
