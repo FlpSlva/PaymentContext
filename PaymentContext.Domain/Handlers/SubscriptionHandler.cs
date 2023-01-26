64451606280d74a9ce2ae3e40aa35a8707b31767
FlpSlva <flpslvadev@gmail.com>
2023-01-26 09:37:30 -0300

    criando handler

---
+++ PaymentContext.Domain/Handlers/SubscriptionHandler.cs
@@ -0,0 +1,88 @@
+﻿using Flunt.Notifications;
+using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Repositories;
+using PaymentContext.Domain.ValuesObjects;
+using PaymentContext.Shared.Commands;
+using PaymentContext.Shared.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Domain.Handlers
+{
+    public class SubscriptionHandler :
+        Notifiable,
+        IHandler<CreateBoletoSubscriptionCommand>
+    {
+        private readonly IStudentRepository _repository;
+
+        public SubscriptionHandler(IStudentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CreateBoletoSubscriptionCommand command)
+        {
+            // Fail Fast Validations
+            command.Validate();
+            if (command.Invalid)
+            {
+                AddNotifications(command);
+                return new CommandResult(false, "Não foi possível realizar seu cadastro");
+
+            }
+
+            // Verificar se documento esta cadastrado
+            if (_repository.DocumentExists(command.Document))
+                AddNotification("Document", "Este CPF já está em uso");
+
+
+            // Verificar se email esta cadastrado
+
+            if (_repository.EmailExists(command.Email))
+                AddNotification("Email", "Este E-mail já está em uso");
+
+            // Gerar os VO 
+
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document, Enums.EDocumentType.CPF);
+            var email = new Email(command.Email);
+            var address = new Address(command.Street, command.Number, command.Neighborhood, command.City, command.State, command.Country, command.ZipCode);
+
+            // Gerar as entidades
+
+            var student = new Student(name, email, document);
+            var subscription = new Subscription(DateTime.Now.AddMonths(1));
+            var payment = new BoletoPayment(
+                command.BarCode,
+                command.BoletoNumber,
+                command.PaidDate,
+                command.ExpireDate,
+                command.Total,
+                command.TotalPaid,
+                address,
+                new Document(command.PayerDocument, command.PayerDocumentType),
+                command.Payer,
+                email
+                );
+            // Relacionamentos
+            subscription.AddPayment(payment);
+            student.AddSubscription(subscription);
+
+            // Agrupar as validacoes
+            AddNotifications(name, email, address, student, subscription, payment);
+
+            // Salvar as informacoes
+            _repository.CreateSubscription(student);
+
+            // Enviar email de boas vindas
+
+            // Retornar informacoes
+
+            return new CommandResult(true, "Assinatura realizada com sucesso");
+        }
+    }
+}
