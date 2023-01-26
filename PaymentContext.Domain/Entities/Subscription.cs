4361369e43007b116aa543d129c4006b4cca32c6
FlpSlva <flpslvadev@gmail.com>
2023-01-26 09:36:52 -0300

    correcoes

--- PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -9,34 +9,36 @@ namespace PaymentContext.Domain.Entities
 {
     public class Subscription : Entity
     {
-        public Subscription( DateTime? expireDate)
+        private IList<Payment> _payments;
+        public Subscription(DateTime? expireDate)
         {
             CreateDate = DateTime.Now;
             LastUpdateDate = DateTime.Now;
             ExpireDate = expireDate;
             Active = true;
-            
+            _payments = new List<Payment>();
+
 
         }
 
-        public DateTime CreateDate {get; private set;}
-        public DateTime LastUpdateDate {get; private set;}
-        public DateTime? ExpireDate {get; private set;}
+        public DateTime CreateDate { get; private set; }
+        public DateTime LastUpdateDate { get; private set; }
+        public DateTime? ExpireDate { get; private set; }
         public bool Active { get; private set; }
-        private List<Payment> Payments { get; set; }
+        public IReadOnlyCollection<Payment> Payments { get { return _payments.ToArray(); } }
 
-        public void AddSubscription(Payment payment)
+        public void AddPayment(Payment payment)
         {
             AddNotifications(new Contract()
                 .Requires()
                 .IsGreaterThan(DateTime.Now, payment.PaidDate, "Subscription.Payments", "A data do pagamento deve ser futura"));
 
-            if(Valid)
-            Payments.Add(payment);
+            if (Valid)
+                _payments.Add(payment);
 
         }
 
-        public void Activate() 
+        public void Activate()
         {
             Active = true;
             LastUpdateDate = DateTime.Now;
