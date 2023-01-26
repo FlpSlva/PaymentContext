4185d9e92fafcf451b6a9fe37c6064b9113a57e8
FlpSlva <flpslvadev@gmail.com>
2023-01-26 09:36:45 -0300

    correcoes

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -10,38 +10,41 @@ namespace PaymentContext.Domain.Entities
 {
     public class Student : Entity
     {
+        private IList<Subscription> _subscriptions;
         public Student(Name name, Email email, Document document)
         {
             Name = name;
             Email = email;
             Document = document;
+            _subscriptions = new List<Subscription>();
 
             AddNotifications(name, document, email);
         }
 
-        public Name Name {get; private set;}
-        public string LastName {get; private set;}
-        public Email Email {get; private set;}
-        public Document Document {get; private set;}
+        public Name Name { get; private set; }
+        public string LastName { get; private set; }
+        public Email Email { get; private set; }
+        public Document Document { get; private set; }
         public Address Address { get; private set; }
-        private List<Subscription> Subscriptions {get; set;}
+        public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
 
         public void AddSubscription(Subscription subscription)
         {
             var hasSubscriptionActive = false;
-            foreach(var sub in Subscriptions)
+            foreach (var sub in Subscriptions)
             {
-                if(sub.Active)
+                if (sub.Active)
                     hasSubscriptionActive = true;
             }
 
-            //AddNotifications(new Contract()
-            //    .Requires()
-            //    .IsFalse(hasSubscriptionActive, "Student.Subscriptions", "Você já tem uma sinatura ativa")
-            //    );
+            AddNotifications(new Contract()
+             .Requires()
+               .IsFalse(hasSubscriptionActive, "Student.Subscriptions", "Você já tem uma sinatura ativa")
+               .IsGreaterThan(0, subscription.Payments.Count, "Student.Subscription", "Esta assinatura não possui pagamentos")
+                );
 
-            if (hasSubscriptionActive)
-                AddNotification("Student.Subscription", "Você já tem uma assinatura ativa");
+            //if (hasSubscriptionActive)
+            //    AddNotification("Student.Subscription", "Você já tem uma assinatura ativa");
 
         }
     }
