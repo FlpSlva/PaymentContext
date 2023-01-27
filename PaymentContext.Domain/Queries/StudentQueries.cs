6b2573eaf5b4d60422dccbceace0c7320764bfa9
FlpSlva <flpslvadev@gmail.com>
2023-01-27 09:35:42 -0300

    create querie

---
+++ PaymentContext.Domain/Queries/StudentQueries.cs
@@ -0,0 +1,19 @@
+﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.ValuesObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Domain.Queries
+{
+    public static class StudentQueries
+    {
+        public static Expression<Func<Student, bool>> GetStudent(string document)
+        {
+            return x => x.Document.Number == document;
+        }
+    }
+}
