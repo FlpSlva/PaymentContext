5ced298f46fa7f9fcc51a27938bbed523d9fc3ac
FlpSlva <flpslvadev@gmail.com>
2023-01-27 09:37:29 -0300

    criando queries de student

---
+++ PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,49 @@
+﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValuesObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Tests.Queries
+{
+    [TestClass]
+    public class StudentQueriesTests
+    {
+        private IList<Student> _students;
+
+        public StudentQueriesTests()
+        {
+            for (var i = 0; i <= 10; i++)
+            {
+                    _students.Add(new Student(
+                    new Name("Aluno", i.ToString()),
+                    new Email(i.ToString() + "batman@gmail.com"),
+                    new Document("1111111111" + i.ToString(), EDocumentType.CPF)
+
+                    ));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnNullWhenDocumentNotExists()
+        {
+            var exp = StudentQueries.GetStudent("123456789010");
+            var student = _students.AsQueryable().Where(exp).FirstOrDefault();  
+
+            Assert.AreEqual(null, student);
+        }
+
+        [TestMethod]
+        public void ShouldReturnStudentWhenDocumentExists()
+        {
+            var exp = StudentQueries.GetStudent("11111111111");
+            var student = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreNotEqual(null, student);
+        }
+    }
+}
