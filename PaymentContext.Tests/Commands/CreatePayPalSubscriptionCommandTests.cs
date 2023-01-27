9598dbcd195e8d768c2c4ae50bcfabb804919ac0
FlpSlva <flpslvadev@gmail.com>
2023-01-27 09:35:56 -0300

    correcao

--- PaymentContext.Tests/Commands/CreatePayPalSubscriptionCommandTests.cs
+++ PaymentContext.Tests/Commands/CreatePayPalSubscriptionCommandTests.cs
@@ -14,7 +14,7 @@ namespace PaymentContext.Tests.Commands
         public void ShouldReturnErrorWhenNameIsInvalid()
         {
             var command = new CreateBoletoSubscriptionCommand();
-            command.FirstName = string.Empty;
+            command.FirstName = "";
 
             command.Validate();
             Assert.AreEqual(false, command.Valid);
